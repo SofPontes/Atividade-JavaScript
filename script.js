ddd1dc4b8c53d7166f6c5294149c24388d1c0122
SofiaPontes <santospontessofia@gmail.com>
2024-10-17 20:34:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function numero(){
+    //solicite um número
+    let numero = prompt("Digite um número");
+}
+if(numero>10){
+alert("Número maior que 10")
+}
+else{
+alert("Número maior que 10")
+}
